0d07e72cb74cbdec19ff09e9a9bb86e726586b05
maannnne <mane.poghosian@gmail.com>
2018-12-05 00:03:25 +0400

    Change Sign In email and password.

--- test/amazon.test.js
+++ test/amazon.test.js
@@ -1,32 +1,32 @@
 const assert = require('assert');
 
 describe('Amazon', () => {
-    describe('Find and print first 15 tablet names which cost less than 70$', () =>{
+    describe('Find and print first 15 tablet names which cost less than 70$', () => {
 
-        it('"Hello. Sign in" text is Correct', () => {
+        it('"Hello. Sign in" text is Correct.', () => {
             browser.url("/");
             const helloSignInText = browser.element("//a/span[contains(@class,'nav-line-1')]").getText();
             assert.equal(helloSignInText, 'Hello. Sign in');
             browser.element("//*[@id = 'nav-link-accountList']").click();
         })
 
-        it('Email lable text is correct', () => {
+        it('Email lable text is correct.', () => {
             browser.waitForExist('//*[@for="ap_email"]', 10000);
             const emailLabel = browser.element("//label[@for = 'ap_email']").getText();
             assert.equal(emailLabel, 'Email (phone for mobile accounts)');
-            browser.setValue('//*[@id="ap_email"]', "mane.poghosian@gmail.com");
+            browser.setValue('//*[@id="ap_email"]', "testester00001@gmail.com");
         })         
 
-        it('Password label text is correct ', () => {
+        it('Password label text is correct.', () => {
             browser.waitForExist('//*[@id="ap_password"]', 10000);
             const passwordLabel = browser.element("//label[@for = 'ap_password']").getText();
-            browser.setValue('//*[@id="ap_password"]', "Amaz0nDranic");
+            browser.setValue('//*[@id="ap_password"]', "Tester00001");
             assert.equal(passwordLabel, 'Password');
             browser.waitForExist(".a-button-input", 10000);
             browser.element("//*[@class = 'a-button-input']").click();
         }) 
         
-        it('Search result exists', () => {
+        it('Search result exists.', () => {
             browser.waitForExist("#twotabsearchtextbox", 10000);
             browser.setValue('//*[@id = "twotabsearchtextbox"]', 'tablet');
             browser.element("//*[@class = 'nav-input']").click(); 
@@ -35,7 +35,7 @@ describe('Amazon', () => {
             assert.equal(exists, true);
         })
 
-        it('Find and print first 15 tablet names, which cost less than 70$ ', () => {
+        it('Find and print first 15 tablet names, which cost less than 70$.', () => {
             browser.waitForExist('#atfResults', 10000);
             const wholePrices = browser.elements('.sx-price-whole').getText();     
             const tabletNames = browser.elements('.s-access-title').getText();
