cc456d3e164a354adf582b831918976d9f3760dd
maannnne <mane.poghosian@gmail.com>
2018-12-04 23:40:36 +0400

    Find and print first 15 tablet names, which cost less than 70$

--- test/amazon.test.js
+++ test/amazon.test.js
@@ -10,29 +10,44 @@ describe('Amazon', () => {
             browser.element("//*[@id = 'nav-link-accountList']").click();
         })
 
-        it('Email lable text is correct', () =>{
+        it('Email lable text is correct', () => {
             browser.waitForExist('//*[@for="ap_email"]', 10000);
             const emailLabel = browser.element("//label[@for = 'ap_email']").getText();
             assert.equal(emailLabel, 'Email (phone for mobile accounts)');
             browser.setValue('//*[@id="ap_email"]', "mane.poghosian@gmail.com");
         })         
 
-        it('Password label text is correct', ()=> {
+        it('Password label text is correct ', () => {
             browser.waitForExist('//*[@id="ap_password"]', 10000);
             const passwordLabel = browser.element("//label[@for = 'ap_password']").getText();
             browser.setValue('//*[@id="ap_password"]', "Amaz0nDranic");
             assert.equal(passwordLabel, 'Password');
             browser.waitForExist(".a-button-input", 10000);
             browser.element("//*[@class = 'a-button-input']").click();
-        })  
+        }) 
         
-        it('Search result exists,', () => {
+        it('Search result exists', () => {
             browser.waitForExist("#twotabsearchtextbox", 10000);
-            browser.setValue('//*[@id = "twotabsearchtextbox"]', 'tadwrewd3a34dtf45let');
+            browser.setValue('//*[@id = "twotabsearchtextbox"]', 'tablet');
             browser.element("//*[@class = 'nav-input']").click(); 
-            let exist = browser.isExisting('#atfResults');
-            console.log('Search result existing status: ' + exist);
-            assert.equal(exist, true);
+            let exists = browser.isExisting('#atfResults');
+            console.log('Search result existing status: ' + exists);
+            assert.equal(exists, true);
+        })
+
+        it('Find and print first 15 tablet names, which cost less than 70$ ', () => {
+            browser.waitForExist('#atfResults', 10000);
+            const wholePrices = browser.elements('.sx-price-whole').getText();     
+            const tabletNames = browser.elements('.s-access-title').getText();
+
+            let count = 15;
+            
+            for(let i = 0; i < wholePrices.length; i++){
+                if(parseInt(wholePrices[i]) < 70 && count > 0){
+                    console.log((i+1) + '. ' + tabletNames[i]);
+                    count--;
+                }
+            }
         })
     })
 })
